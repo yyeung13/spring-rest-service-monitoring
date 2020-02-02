aa2c7f50a66cbe6558e4528f4cb731211414eb1f
yyeung13 <33860110+yyeung13@users.noreply.github.com>
2020-02-03 01:15:41 +0800

    Update GreetingController.java

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Good Morning, %s!";
+	private static final String template = "Good Evening, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
